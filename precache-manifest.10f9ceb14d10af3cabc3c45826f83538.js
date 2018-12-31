ee30ce0d879b24a23c09611c9fde8c85b60912fb
Yoann Cribier <yoann_cribier@hotmail.com>
2018-12-31 14:46:41 +0100

    Updates

---
+++ precache-manifest.10f9ceb14d10af3cabc3c45826f83538.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7e1edaba771e80cbc7c",
+    "url": "/6packs/static/js/runtime~main.f7e1edab.js"
+  },
+  {
+    "revision": "4658a0f0fdd3a5e777f8",
+    "url": "/6packs/static/js/main.4658a0f0.chunk.js"
+  },
+  {
+    "revision": "2f402f46b27399c4aa5c",
+    "url": "/6packs/static/js/1.2f402f46.chunk.js"
+  },
+  {
+    "revision": "4658a0f0fdd3a5e777f8",
+    "url": "/6packs/static/css/main.fcc70271.chunk.css"
+  },
+  {
+    "revision": "6c79119a44243d9c071c227bdc0b724c",
+    "url": "/6packs/index.html"
+  }
+];
