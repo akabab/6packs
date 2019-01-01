489e20897731d4e1e4f8a657dfcf3d2025ff20d9
Yoann Cribier <yoann_cribier@hotmail.com>
2019-01-01 20:20:14 +0100

    Updates

--- precache-manifest.791697f52201aeb331bb4491148a4b16.js
+++ precache-manifest.791697f52201aeb331bb4491148a4b16.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/6packs/static/js/runtime~main.f7e1edab.js"
   },
   {
-    "revision": "d0b23885bed61b1b8e8b",
-    "url": "/6packs/static/js/main.d0b23885.chunk.js"
+    "revision": "8aea9c420aa3d2fc31d7",
+    "url": "/6packs/static/js/main.8aea9c42.chunk.js"
   },
   {
     "revision": "1ef4b09c3e5e9eb1aef9",
     "url": "/6packs/static/js/1.1ef4b09c.chunk.js"
   },
   {
-    "revision": "d0b23885bed61b1b8e8b",
+    "revision": "8aea9c420aa3d2fc31d7",
     "url": "/6packs/static/css/main.b07d9899.chunk.css"
   },
   {
-    "revision": "279351527dd324629dda772210c32f68",
+    "revision": "2fa3c8af73aab52c550c33e30950f957",
     "url": "/6packs/index.html"
   }
 ];
